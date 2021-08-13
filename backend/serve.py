8f0b37ff27c288ea7214668327079f833d97afd3
Peter Kouvaris <kouvaris.peter@gmail.com>
2021-08-13 09:05:56 -0400

    cleaning up homepage

--- backend/serve.py
+++ backend/serve.py
@@ -5,18 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    template_variables = {
-        "page_nme": "@/",
-        "pages": [
-            {
-                "page_name": "test page 1",
-                "content": """
-        Twoooooooooooo
-        """,
-            },
-            {"page_name": "test page 2"},
-        ],
-    }
     return render_template("./homepage.html", **template_variables)
 
 
